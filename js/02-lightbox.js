8a1905eec1b5cc5212cb1faecd9cb1db2524772b
Stanislav-Zhuk <steys.ua@gmail.com>
2023-03-05 12:36:48 +0200

    update task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Gallery element markup
+/*
+<a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>
+*/
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryMarkup = object => object.map(
+		({ original, preview, description }) =>
+			`<a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt='${description}' />
+      </a>`,
+);
+
+galleryEl.insertAdjacentHTML("afterbegin", createGalleryMarkup(galleryItems).join(""));
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+lightbox.options.captionsData = "alt";
+lightbox.options.captionDelay = 250;
